3331fbef1be02560e44f5319c6392b018fbb7ac7
Dongho Gwak <wing.flower.k@gmail.com>
2023-12-27 18:05:26 +0900

    feat: Network 데이터 연결 정의

--- lib/repository/image_item_repository.dart
+++ lib/repository/image_item_repository.dart
@@ -1,4 +1,25 @@
+import 'package:image_search_app_ver1/data_source/pixabay_api.dart';
+import 'package:image_search_app_ver1/mapper/image_mapper.dart';
 import 'package:image_search_app_ver1/model/image_item.dart';
+
+
+abstract interface class ImageItemRepository {
+  Future<List<ImageItem>> getImageItems(String query);
+}
+
+class PixabayImageItemRepository implements ImageItemRepository {
+  final _api = PixabayApi();
+
+  @override
+  Future<List<ImageItem>> getImageItems(String query) async {
+    final dto = await _api.getImagesResult(query);
+
+    if (dto.hits == null) {
+      return [];
+    }
+    return dto.hits!.map((e) => e.toImageItem()).toList();
+  }
+}
 
 class MockImageItemRepository {
   Future<List<ImageItem>> getImageItems(String query) async {
