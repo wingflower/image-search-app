63847bcde7b70bffe9d13b4bffb71bdd706f075b
Dongho Gwak <wing.flower.k@gmail.com>
2023-12-27 17:11:07 +0900

    feat: 화면 정의 완료(데이터 연동 전)

--- lib/screen/image_search_screen.dart
+++ lib/screen/image_search_screen.dart
@@ -8,12 +8,59 @@ class ImageSearchScreen extends StatefulWidget {
 }
 
 class _ImageSearchScreenState extends State<ImageSearchScreen> {
+  final List<String> stringList = ['111', '222'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TextField(
-        decoration: InputDecoration(
-          suffixIcon: Icon(Icons.search),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: Color(0xFF4FB6B2),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: Color(0xFF4FB6B2),
+                    ),
+                  ),
+                  suffixIcon: Icon(
+                    Icons.search,
+                    color: Color(0xFF4FB6B2),
+                  ),
+                  hintText: 'Search',
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: 100, // 총 아이템 수
+                  itemBuilder: (context, index) {
+                    return Container(
+                      color: Colors.purple,
+                    );
+                  },
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // 열
+                    crossAxisSpacing: 10, // 열 공백
+                    mainAxisSpacing: 10, // 행 공백
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
